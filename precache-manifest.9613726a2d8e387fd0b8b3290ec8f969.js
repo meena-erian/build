90c966f91ebf7df0b36d929bc3f704cbfd0332db
Meena Erian <meena5.erian@gmail.com>
2020-03-26 10:39:18 +0200

    Initial commit

---
+++ precache-manifest.9613726a2d8e387fd0b8b3290ec8f969.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe9e29e3dba2fe0b485dc7b33de7fee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a80e16e15c838d4ab537",
+    "url": "/static/css/main.74959bab.chunk.css"
+  },
+  {
+    "revision": "d1432b0d14a703c0a246",
+    "url": "/static/js/2.4536aaa0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4536aaa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a80e16e15c838d4ab537",
+    "url": "/static/js/main.b5bad113.chunk.js"
+  },
+  {
+    "revision": "081b08c61c7759989c9c",
+    "url": "/static/js/runtime-main.e0d54d3e.js"
+  }
+]);
